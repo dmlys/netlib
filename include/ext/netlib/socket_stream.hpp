a5fb07a7bb612c0e5f5c2c46ded87cdc2dfd9a4a
Dmitry Lisachenko <xdemonx07@gmail.com>
2017-02-08 21:06:31 +0300

    initial commit

---
+++ include/ext/netlib/socket_stream.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <ext/iostreams/socket_stream.hpp>
+
+namespace simple_mail
+{
+	typedef ext::socket_stream    socket_stream;
+	typedef ext::socket_streambuf socket_streambuf;
+}
