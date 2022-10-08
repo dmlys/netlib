d85b92559200b578df94d31a94be102247d6cf71
Dmitry Lisachenko <xdemonx07@gmail.com>
2022-10-08 16:45:05 +0300

    listener added static functions working with handles

--- include/ext/net/listener.hpp
+++ include/ext/net/listener.hpp
@@ -22,44 +22,82 @@ namespace ext::net
 	};
 
 
-	/// simple socket listener class for use in very simple socket server applications,
-	/// for more complex ones - use more appropriate libraries, for example boost::asio
+	/// simple socket listener wrapper class
 	class listener
 	{
 	private:
 		socket_handle_type m_listening_socket = -1;
 
 	public:
+		// free functions for direct calls on listener handle
+		
 		/// check if socket is listen state(listen method was called),
 		/// internally calls ::getsockopt(..., SOL_SOCKET SO_ACCEPTCONN, ...).
 		/// Throws std::system_error in case or errors
-		bool is_listening() const;
-		bool is_socket() const;
+		static bool is_listening(socket_handle_type handle);
+		static bool is_socket(socket_handle_type handle);
+		
+		/// binds this listener to given address and port.
+		/// first address is resolved via ::getaddrinfo with hints:
+		///    hint.ai_flags = AI_PASSIVE | AI_ADDRCONFIG | AI_V4MAPPED | AI_ALL;
+		///    hint.ai_family = af;
+		///    hint.ai_protocol = IPPROTO_TCP;
+		///    hint.ai_socktype = SOCK_STREAM;
+		/// then socket from created and bound from/to resolved address.
+		/// Also setsockopt with SO_REUSEADDR is called.
+		/// Throws std::system_error in case or errors
+		static void bind(socket_handle_type & handle, const char * ipaddr, unsigned short port, int af = af_unspec);
+		/// binds this listener to given address, protocol and family is taken from address
+		static void bind(socket_handle_type & handle, sockaddr * sockaddr, socklen_t addrlen, int socktype, int protocol = 0);
+		
+		static void bind(socket_handle_type & handle, unsigned short port, int af = af_unspec) { return bind(handle, nullptr, port, af); }
+		static void bind(socket_handle_type & handle, const std::string & ipaddr, unsigned short port, int af = af_unspec)
+			{ return bind(handle, ipaddr.empty() ? nullptr : ipaddr.c_str(), port, af); }
+		
+		/// calls ::listen and checks result,
+		/// throws std::system_error in case or errors
+		static void listen(socket_handle_type handle, int backlog);
+		/// calls ::accept and checks result,
+		/// throws std::system_error in case or errors
+		static socket_handle_type accept(socket_handle_type handle);
+		/// calls ::shutdown and checks result,
+		/// throws std::system_error in case or errors
+		static void shutdown(socket_handle_type handle);
+		
+		// getsockname family functions are availible from socket_base.hpp
+		
+	public:
+		/// check if socket is listen state(listen method was called),
+		/// internally calls ::getsockopt(..., SOL_SOCKET SO_ACCEPTCONN, ...).
+		/// Throws std::system_error in case or errors
+		bool is_listening() const { return is_listening(m_listening_socket); }
+		bool is_socket() const { return is_socket(m_listening_socket); }
 		explicit operator bool() const { return is_socket(); }
 
 		/// underlying socket handle
 		socket_handle_type handle() const noexcept { return m_listening_socket; }
+		socket_handle_type release()      noexcept { return std::exchange(m_listening_socket, invalid_socket); }
 
 	public:
 		/// calls ::getsockname(handle(), addr, namelen),
 		/// throws std::system_error in case or errors
-		void getsockname(sockaddr_type * addr, socklen_t * addrlen) const;
+		void getsockname(sockaddr_type * addr, socklen_t * addrlen) const { return ext::net::getsockname(m_listening_socket, addr, addrlen); }
 		/// returns socket endpoint address port as $addr:$port string(calls getsockname)
 		/// throws std::system_error in case or errors
-		std::string sock_endpoint() const;
+		std::string sock_endpoint() const { return ext::net::sock_endpoint(m_listening_socket); }
 		/// safe version of sock_endpoint - does not throws exception(except for std::bad_alloc),
 		/// in case of error returns <ERR:code>. For example - <ENOTCONN:107>
-		std::string sock_endpoint_noexcept() const;
+		std::string sock_endpoint_noexcept() const { return ext::net::sock_endpoint_noexcept(m_listening_socket); }
 		/// returns socket endpoint address and port(calls getsockname).
 		/// throws std::system_error in case or errors
-		void sock_name(std::string & name, unsigned short & port) const;
-		auto sock_name() const -> std::pair<std::string, unsigned short>;
+		void sock_name(std::string & name, unsigned short & port) const  { return ext::net::sock_name(m_listening_socket, name, port); }
+		auto sock_name() const -> std::pair<std::string, unsigned short> { return ext::net::sock_name(m_listening_socket); }
 		/// returns socket endpoint address(calls getsockname).
 		/// throws std::system_error in case or errors
-		std::string sock_address() const;
+		std::string sock_address() const { return ext::net::sock_address(m_listening_socket); }
 		/// returns socket endpoint port(calls getsockname).
 		/// throws std::system_error in case or errors
-		unsigned short sock_port() const;
+		unsigned short sock_port() const { return ext::net::sock_port(m_listening_socket); }
 
 	public:
 		/// binds this listener to given address and port.
@@ -71,23 +109,25 @@ namespace ext::net
 		/// then socket from created and bound from/to resolved address.
 		/// Also setsockopt with SO_REUSEADDR is called.
 		/// Throws std::system_error in case or errors
-		void bind(unsigned short port, int af = af_unspec) { bind("", port, af); }
-		void bind(std::string ipaddr, unsigned short port, int af = af_unspec);
+		void bind(const char * ipaddr, unsigned short port, int af = af_unspec)           { return bind(m_listening_socket, ipaddr, port, af); }
 		/// binds this listener to given address, protocol and family is taken from address
-		void bind(sockaddr * sockaddr, socklen_t addrlen, int socktype, int protocol = 0);
+		void bind(sockaddr * sockaddr, socklen_t addrlen, int socktype, int protocol = 0) { return bind(m_listening_socket, sockaddr, addrlen, socktype, protocol); }
+		
+		void bind(unsigned short port, int af = af_unspec)                                { return bind(m_listening_socket, port, af); }
+		void bind(const std::string & ipaddr, unsigned short port, int af = af_unspec)    { return bind(m_listening_socket, ipaddr, port, af); }
 		
 		/// calls ::listen and checks result,
 		/// throws std::system_error in case or errors
-		void listen(int backlog);
+		void listen(int backlog) { return listen(m_listening_socket, backlog); }
 		/// calls ::accept and checks result,
 		/// throws std::system_error in case or errors
-		socket_handle_type accept();
+		socket_handle_type accept() { return accept(m_listening_socket); }
 		/// calls ::shutdown and checks result,
 		/// throws std::system_error in case or errors
 		void shutdown();
 		/// calls ::close and checks result,
 		/// throws std::system_error in case or errors
-		void close();
+		void close() noexcept;
 
 	public:
 		listener() = default;
--- src/listener.cpp
+++ src/listener.cpp
@@ -21,96 +21,31 @@ namespace ext::net
 		throw listener_exception(std::move(sock_endpoint), last_socket_error_code(), errmsg);
 	}
 
-	bool listener::is_listening() const
+	bool listener::is_listening(socket_handle_type handle)
 	{
-		if (not m_listening_socket) return false;
+		if (handle == invalid_socket) return false;
 
 		int enabled = 0;
 		socklen_t len = sizeof(enabled);
-		int res = ::getsockopt(m_listening_socket, SOL_SOCKET, SO_ACCEPTCONN, reinterpret_cast<char *>(&enabled), &len);
+		int res = ::getsockopt(handle, SOL_SOCKET, SO_ACCEPTCONN, reinterpret_cast<char *>(&enabled), &len);
 		if (res != 0) throw_last_socket_error("ext::net::listener::is_listening: ::getsockopt SO_ACCEPTCONN failed");
 
 		return enabled;
 	}
 
-	bool listener::is_socket() const
+	bool listener::is_socket(socket_handle_type handle)
 	{
-		return m_listening_socket != invalid_socket;
+		return handle != invalid_socket;
 	}
 
-	void listener::getsockname(sockaddr_type * addr, socklen_t * addrlen) const
-	{
-		if (m_listening_socket == invalid_socket)
-			throw std::runtime_error("ext::net::listener::getsockname: bad socket");
 
-		sockoptlen_t * so_addrlen = reinterpret_cast<sockoptlen_t *>(addrlen);
-		auto res = ::getsockname(m_listening_socket, addr, so_addrlen);
-		if (res != 0) throw_last_socket_error("ext::net::listener::getsockname: ::getsockname failed");
-	}
-
-	std::string listener::sock_endpoint() const
-	{
-		sockaddr_storage addrstore;
-		socklen_t addrlen = sizeof(addrstore);
-		auto * addr = reinterpret_cast<sockaddr *>(&addrstore);
-		getsockname(addr, &addrlen);
-
-		return sock_addr(addr);
-	}
-	
-	std::string listener::sock_endpoint_noexcept() const
-	{
-		sockaddr_storage addrstore;
-		socklen_t addrlen = sizeof(addrstore);
-		auto * addr = reinterpret_cast<sockaddr *>(&addrstore);
-		getsockname(addr, &addrlen);
-
-		return sock_addr_noexcept(addr);
-	}
-
-	unsigned short listener::sock_port() const
-	{
-		sockaddr_storage addrstore;
-		socklen_t addrlen = sizeof(addrstore);
-		auto * addr = reinterpret_cast<sockaddr *>(&addrstore);
-		getsockname(addr, &addrlen);
-
-		// both sockaddr_in6 and sockaddr_in have port member on same offset
-		auto port = reinterpret_cast<sockaddr_in6 *>(addr)->sin6_port;
-		return ntohs(port);
-	}
-
-	void listener::sock_name(std::string & name, unsigned short & port) const
-	{
-		sockaddr_storage addrstore;
-		socklen_t addrlen = sizeof(addrstore);
-		auto * addr = reinterpret_cast<sockaddr *>(&addrstore);
-		getsockname(addr, &addrlen);
-
-		inet_ntop(addr, name, port);
-	}
-
-	auto listener::sock_name() const -> std::pair<std::string, unsigned short>
-	{
-		std::pair<std::string, unsigned short> res;
-		sock_name(res.first, res.second);
-		return res;
-	}
-
-	std::string listener::sock_address() const
-	{
-		std::string addr; unsigned short port;
-		sock_name(addr, port);
-		return addr;
-	}
-
-	void listener::bind(std::string ipaddr, unsigned short port, int af)
+	void listener::bind(socket_handle_type & handle, const char * ipaddr, unsigned short port, int af)
 	{
 		addrinfo_type hints;
 
 		ext::itoa_buffer<unsigned short> service_buffer;
 		auto * service = ext::itoa(port, service_buffer);
-		auto * host = ipaddr.empty() ? nullptr : ipaddr.c_str();
+		auto * host = ipaddr;
 
 		// AI_PASSIVE - Socket address is intended for `bind'.
 		// AI_ADDRCONFIG - Use configuration of this host to choose returned address type..
@@ -131,48 +66,48 @@ namespace ext::net
 		hints.ai_socktype = SOCK_STREAM;
 
 		std::error_code err;
-		addrinfo_uptr addrinfo = getaddrinfo(host, service, &hints, err);
+		addrinfo_uptr addrinfo = ext::net::getaddrinfo(host, service, &hints, err);
 		if (err) throw std::system_error(err, "ext::net::listener::bind: ::getaddrinfo failed");
 
-		bind(addrinfo->ai_addr, addrinfo->ai_addrlen, addrinfo->ai_socktype, addrinfo->ai_protocol);
+		bind(handle, addrinfo->ai_addr, addrinfo->ai_addrlen, addrinfo->ai_socktype, addrinfo->ai_protocol);
 	}
 	
-	void listener::bind(sockaddr * sockaddr, socklen_t addrlen, int socktype, int protocol/* = 0 */)
+	void listener::bind(socket_handle_type & handle, sockaddr * sockaddr, socklen_t addrlen, int socktype, int protocol/* = 0 */)
 	{
-		m_listening_socket = ::socket(sockaddr->sa_family, socktype, protocol);
-		if (m_listening_socket == invalid_socket) throw_last_socket_error("ext::net::listener::bind: ::socket failed");
+		auto shandle = ::socket(sockaddr->sa_family, socktype, protocol);
+		if (shandle == invalid_socket) throw_last_socket_error("ext::net::listener::bind: ::socket failed");
 
-		//int enabled = 0;
-		//res = ::setsockopt(m_listening_socket, IPPROTO_IPV6, IPV6_V6ONLY, reinterpret_cast<const char *>(&enabled), sizeof(enabled));
-		//if (res != 0) throw_last_socket_error("::setsockopt IPV6_V6ONLY failed");
+		socket_uhandle suhandle(shandle);
 
 		int res, enabled = 1;
-		res = ::setsockopt(m_listening_socket, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char *>(&enabled), sizeof(enabled));
+		res = ::setsockopt(shandle, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char *>(&enabled), sizeof(enabled));
 		if (res != 0) throw_last_socket_error("ext::net::listener::bind: ::setsockopt SO_REUSEADDR failed");
 
 		std::string sock_endpoint = sock_addr_noexcept(sockaddr);
-		res = ::bind(m_listening_socket, sockaddr, addrlen);
+		res = ::bind(shandle, sockaddr, addrlen);
 		if (res != 0) throw_last_listener_error(sock_endpoint, "ext::net::listener::bind: ::bind failed");
+		
+		handle = suhandle.release();
 	}
 
-	void listener::listen(int backlog)
+	void listener::listen(socket_handle_type handle, int backlog)
 	{
-		auto endpoint = sock_endpoint();
-		int res = ::listen(m_listening_socket, backlog);
+		auto endpoint = ext::net::sock_endpoint(handle);
+		int res = ::listen(handle, backlog);
 		if (res < 0) throw_last_listener_error(endpoint, "ext::net::listener::listen: ::listen failed");
 	}
 
-	socket_handle_type listener::accept()
+	socket_handle_type listener::accept(socket_handle_type handle)
 	{
-		socket_handle_type sock = ::accept(m_listening_socket, nullptr, nullptr);
+		socket_handle_type sock = ::accept(handle, nullptr, nullptr);
 		if (sock == invalid_socket) throw_last_socket_error("ext::net::listener::accept: ::accept failed");
 		
 		return sock;
 	}
 	
-	void listener::shutdown()
+	void listener::shutdown(socket_handle_type handle)
 	{
-		if (m_listening_socket == invalid_socket) return;
+		if (handle == invalid_socket) return;
 
 #if BOOST_OS_WINDOWS
 		constexpr int how = SD_BOTH;
@@ -180,12 +115,11 @@ namespace ext::net
 		constexpr int how = SHUT_RDWR;
 #endif
 
-		int res = ::shutdown(m_listening_socket, how);
+		int res = ::shutdown(handle, how);
 		if (res != 0) throw_last_socket_error("shutdown failed");
-		//assert(res == 0);
 	}
 
-	void listener::close()
+	void listener::close() noexcept
 	{
 		if (m_listening_socket == invalid_socket) return;
 
